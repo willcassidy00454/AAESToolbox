01e84d406bd0f244f7455c017d8252010f28f152
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2024-04-29 11:46:19 +0100

    Renaming and reorganising

--- Src/AutomatedRIRGenerator.m
+++ Src/AutomatedRIRGenerator.m
@@ -8,9 +8,12 @@ sample_rate = 48000;
 bit_depth = 24;
 
 % Room dimensions in metres [x y z; x y z; etc.]
-rooms = [5.7    7.35    2.5;
-         8.74   17      5.5;
-         19.52  30.83   15];
+room_dims = [5.7    7.35    2.5;
+             8.74   17      5.5;
+             19.52  30.83   15];
+
+absorptions_dir = "Example Absorption Coefficients/";
+transducer_coords_dir = "Example Transducer Coordinates/";
 
 % Wall absorption coefficients
 % Read "Example Absorption Coefficients/Absorption Coefficients Info.txt" for
@@ -18,20 +21,20 @@ rooms = [5.7    7.35    2.5;
 alphas = zeros(7, 6, 3);
 
 for alpha_set = 1:3
-    alphas(:, :, alpha_set) = readmatrix("Example Absorption Coefficients/alpha_set_" + alpha_set + ".dat");
+    alphas(:, :, alpha_set) = readmatrix(absorptions_dir + "alpha_set_" + alpha_set + ".dat");
 end
 
 % Currently assumes there is only one source and receiver per room
 % These use the dimensions (room number, axis)
 % [x y z; x y z; x y z]
 % The sources are 2/3 along the x and y axes and 1.2m high
-src_sets = [2*rooms(1,1)/3 2*rooms(1,2)/3 1.2
-            2*rooms(2,1)/3 2*rooms(2,2)/3 1.2
-            2*rooms(3,1)/3 2*rooms(3,2)/3 1.2];
+src_sets = [2*room_dims(1,1)/3 2*room_dims(1,2)/3 1.2
+            2*room_dims(2,1)/3 2*room_dims(2,2)/3 1.2
+            2*room_dims(3,1)/3 2*room_dims(3,2)/3 1.2];
 % The receivers are 1/3 along the x and y axes and 1.2m high
-rec_sets = [rooms(1,1)/3 rooms(1,2)/3 1.2
-            rooms(2,1)/3 rooms(2,2)/3 1.2
-            rooms(3,1)/3 rooms(3,2)/3 1.2];
+rec_sets = [room_dims(1,1)/3 room_dims(1,2)/3 1.2
+            room_dims(2,1)/3 room_dims(2,2)/3 1.2
+            room_dims(3,1)/3 room_dims(3,2)/3 1.2];
 
 % ls_sets_... are arranged with dimensions: (ls number, axis, room number)
 % These aren't concatenated due to their dimensions
@@ -46,13 +49,13 @@ mic_sets_16ch = zeros(16,3,3);
 
 % Load coordinates
 for room_num = 1:3
-    ls_sets_8ch(:,:,room_num) = readmatrix("Example Transducer Coordinates/ls_positions_room_" + room_num + "_8ch.dat");
-    ls_sets_12ch(:,:,room_num) = readmatrix("Example Transducer Coordinates/ls_positions_room_" + room_num + "_12ch.dat");
-    ls_sets_16ch(:,:,room_num) = readmatrix("Example Transducer Coordinates/ls_positions_room_" + room_num + "_16ch.dat");
+    ls_sets_8ch(:,:,room_num) = readmatrix(transducer_coords_dir + "ls_positions_room_" + room_num + "_8ch.dat");
+    ls_sets_12ch(:,:,room_num) = readmatrix(transducer_coords_dir + "ls_positions_room_" + room_num + "_12ch.dat");
+    ls_sets_16ch(:,:,room_num) = readmatrix(transducer_coords_dir + "ls_positions_room_" + room_num + "_16ch.dat");
     
-    mic_sets_8ch(:,:,room_num) = readmatrix("Example Transducer Coordinates/mic_positions_room_" + room_num + "_8ch.dat");
-    mic_sets_12ch(:,:,room_num) = readmatrix("Example Transducer Coordinates/mic_positions_room_" + room_num + "_12ch.dat");
-    mic_sets_16ch(:,:,room_num) = readmatrix("Example Transducer Coordinates/mic_positions_room_" + room_num + "_16ch.dat");
+    mic_sets_8ch(:,:,room_num) = readmatrix(transducer_coords_dir + "mic_positions_room_" + room_num + "_8ch.dat");
+    mic_sets_12ch(:,:,room_num) = readmatrix(transducer_coords_dir + "mic_positions_room_" + room_num + "_12ch.dat");
+    mic_sets_16ch(:,:,room_num) = readmatrix(transducer_coords_dir + "mic_positions_room_" + room_num + "_16ch.dat");
 end
 
 if uses_parallel_processing
@@ -65,11 +68,11 @@ if uses_parallel_processing
         disp(4 + 4 * channel_count_index + "-channel AAES...");
         
         if channel_count_index == 1
-            GenerateRIRs(rooms, alphas, src_sets, rec_sets, ls_sets_8ch, mic_sets_8ch, sample_rate, bit_depth);
+            GenerateRIRsParallel(room_dims, alphas, src_sets, rec_sets, ls_sets_8ch, mic_sets_8ch, sample_rate, bit_depth);
         elseif channel_count_index == 2
-            GenerateRIRs(rooms, alphas, src_sets, rec_sets, ls_sets_12ch, mic_sets_12ch, sample_rate, bit_depth);
+            GenerateRIRsParallel(room_dims, alphas, src_sets, rec_sets, ls_sets_12ch, mic_sets_12ch, sample_rate, bit_depth);
         else
-            GenerateRIRs(rooms, alphas, src_sets, rec_sets, ls_sets_16ch, mic_sets_16ch, sample_rate, bit_depth);
+            GenerateRIRsParallel(room_dims, alphas, src_sets, rec_sets, ls_sets_16ch, mic_sets_16ch, sample_rate, bit_depth);
         end
     end
     
@@ -80,16 +83,25 @@ else
         disp(4 + 4 * channel_count_index + "-channel AAES...");
         
         if channel_count_index == 1
-            GenerateRIRs(rooms, alphas, src_sets, rec_sets, ls_sets_8ch, mic_sets_8ch, sample_rate, bit_depth);
+            GenerateRIRs(room_dims, alphas, src_sets, rec_sets, ls_sets_8ch, mic_sets_8ch, sample_rate, bit_depth);
         elseif channel_count_index == 2
-            GenerateRIRs(rooms, alphas, src_sets, rec_sets, ls_sets_12ch, mic_sets_12ch, sample_rate, bit_depth);
+            GenerateRIRs(room_dims, alphas, src_sets, rec_sets, ls_sets_12ch, mic_sets_12ch, sample_rate, bit_depth);
         else
-            GenerateRIRs(rooms, alphas, src_sets, rec_sets, ls_sets_16ch, mic_sets_16ch, sample_rate, bit_depth);
+            GenerateRIRs(room_dims, alphas, src_sets, rec_sets, ls_sets_16ch, mic_sets_16ch, sample_rate, bit_depth);
         end
     end
 end
 
 function GenerateRIRs(rooms, alphas, src_sets, rec_sets, ls_sets, mic_sets, sample_rate, bit_depth)
+    for room_num = 1:size(rooms, 1)
+        disp("Room " + room_num + "...");
+    
+        current_config = RoomWithAAES(rooms(room_num,:), alphas, src_sets(room_num,:), rec_sets(room_num,:), ls_sets(:,:,room_num), mic_sets(:,:,room_num), sample_rate, bit_depth);
+        current_config.GenerateSystemIRs();
+    end
+end
+
+function GenerateRIRsParallel(rooms, alphas, src_sets, rec_sets, ls_sets, mic_sets, sample_rate, bit_depth)
     parfor room_num = 1:size(rooms, 1)
         disp("Room " + room_num + "...");
     
