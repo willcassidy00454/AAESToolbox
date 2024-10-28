6fbadf1aa551aa27197cc2fb36783b6c634363ba
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2024-10-28 17:16:57 +0000

    Clean up

--- Src/Audio Generation/GenerateNoiseBasedIR.m
+++ Src/Audio Generation/GenerateNoiseBasedIR.m
@@ -5,9 +5,9 @@
 
 % This matrix maps reverberation times to their specific cases
 % room num x alpha set
-rt_map = [1.08];%[0.25 0.51 1.14
-                   % 0.54 1.08 2.11
-                   % 1.19 2.30 3.90];
+rt_map = [0.25 0.51 1.14
+           0.54 1.08 2.11
+           1.19 2.30 3.90];
 
 rt_factors = [1.5 2.5 3 3.5];
 
@@ -23,12 +23,13 @@ cutoff = 20;
 [zhi,phi,khi] = butter(8,2*cutoff/sample_rate,"high");
 sos_hpf = zp2sos(zhi,phi,khi);
 
+parent_dir = "/Users/you/your/path/";
+
 for room_num = 1:size(rt_map, 1)
     for alpha_set = 1:size(rt_map, 2)
         for rt_factor_index = 1:size(rt_factors, 2)
             rt_factor = rt_factors(rt_factor_index);
-            output_dir = "/Users/willcassidy/Documents/PhD/MATLAB/AAES Model/Pink Reverberator IRs/Decaying Noise Ch["+num_channels+"] Room[2] AlphaSet[2] RTFactor["+rt_factor+"]/";
-            % output_dir = "/Users/willcassidy/Documents/PhD/MATLAB/AAES Model/Pink Reverberator IRs/Decaying Noise Ch["+num_channels+"] Room["+room_num+"] AlphaSet["+alpha_set+"] RTFactor["+rt_factor+"]/";
+            output_dir = parent_dir + "Pink Reverberator IRs/Decaying Noise Ch["+num_channels+"] Room["+room_num+"] AlphaSet["+alpha_set+"] RTFactor["+rt_factor+"]/";
             mkdir(output_dir);
 
             GenerateAndSaveIR(rt_map(room_num, alpha_set) * rt_factor, sample_rate, output_dir, num_channels, bit_depth, sos_hpf, is_pink, hpf_enabled);
@@ -37,11 +38,7 @@ for room_num = 1:size(rt_map, 1)
 end
 
 function GenerateAndSaveIR(reverb_time, sample_rate, file_directory, num_channels, bit_depth, sos_hpf, is_pink, hpf_enabled)
-    % reverb_time = 1;
     tail_length_seconds = reverb_time; % Time after 60dB of decay
-    % bit_depth = 24;
-    % file_directory = "/Users/willcassidy/Documents/PhD/MATLAB/Reverberator IRs/Decaying Noise Ch[8] Room[1] AlphaSet[3] SampleRate[48000]/";
-    % num_channels = 8;
     
     % Saves a diagonal of uncorrelated noise (x_1_1, X_2_2 etc.)
     for channel = 1:num_channels
@@ -57,9 +54,9 @@ function y = GetExpDecayCoeff(time_seconds, reverb_time)
 end
 
 function ir = GenerateIR(reverb_time, tail_length_seconds, sample_rate, sos_hpf, is_pink, hpf_enabled)
-    ir = 1; % If reverb time is 0 sec, output this impulse
+    ir = 1; % If reverb time is 0 sec, output a unit impulse
     
-    if reverb_time ~= 0.0
+    if reverb_time > 0.0
         num_samples = floor((reverb_time + tail_length_seconds) * sample_rate);
 
         if is_pink
